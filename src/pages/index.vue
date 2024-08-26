1cf244c48c944e89698c2b427e647613d5fb04de
unknown <shevaardhana26@gmail.com>
2024-08-27 06:18:14 +0700

    second commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <HelloWorld />
+  <h5>Link Tugas</h5>
+  <ul>
+    <li><a href="http://localhost:3000/first">Go to first task</a></li>
+    <li><a href="http://localhost:3000/second">Go to second task</a></li>
+    <li><a href="http://localhost:3000/third">Go to third task</a></li>
+  </ul>
 </template>
 
 <script setup>
